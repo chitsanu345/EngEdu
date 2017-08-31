3ca5eac6d713be00205ce7178b295a79ae8574cc
chitsanu345 <chitsanu345@gmail.com>
2017-08-31 22:55:53 +0700

    fig

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Year 255x semester 1 , I am an Engedu freshman.");
-            Console.WriteLine("Year 255x semester 2 , I learn to program in C sharp.");
+            Console.WriteLine("Year 255x semester 2 , I learn to program in C dgdfgshdfgdf");
             Console.WriteLine("Year 2560 semester 1 , I start my programmer life.");
         }
     }
